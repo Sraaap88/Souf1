eedf9c2572164d7475291f0c3244d27637b81313
Sraaap88 <jonaprise@yahoo.ca>
2025-07-02 09:59:02 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/souffleforcetest/MainActivity.kt
+++ app/src/main/java/com/example/souffleforcetest/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.example.souffleforcetest
 
+import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        try {
-            Toast.makeText(this, "App démarrée !", Toast.LENGTH_LONG).show()
-            setContentView(R.layout.activity_main)
-            Toast.makeText(this, "Layout chargé !", Toast.LENGTH_LONG).show()
-        } catch (e: Exception) {
-            Toast.makeText(this, "ERREUR: ${e.message}", Toast.LENGTH_LONG).show()
-        }
+        // Pas de layout, juste un écran noir
     }
 }
