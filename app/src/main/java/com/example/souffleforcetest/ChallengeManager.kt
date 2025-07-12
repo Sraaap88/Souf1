6ed6991d8c65baaa028d6d52ee7ed21d142566ef
Sraaap88 <jonaprise@yahoo.ca>
2025-07-12 18:51:24 -0400

    Update ChallengeManager.kt

--- app/src/main/java/com/example/souffleforcetest/ChallengeManager.kt
+++ app/src/main/java/com/example/souffleforcetest/ChallengeManager.kt
@@ -97,6 +97,10 @@ class ChallengeManager(private val context: Context? = null) {
     init {
         // Marguerite toujours débloquée par défaut
         unlockedFlowers.add(UnlockedFlower("MARGUERITE", "Disponible par défaut"))
+        // FORCE L'IRIS POUR TESTER
+        unlockedFlowers.add(UnlockedFlower("ROSE", "Test"))
+        unlockedFlowers.add(UnlockedFlower("LUPIN", "Test"))
+        unlockedFlowers.add(UnlockedFlower("IRIS", "Test"))
         loadChallengeProgress()
     }
     
